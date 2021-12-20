9d21fe6316e200fd919d0a85b1a27bd97b428cfd
euclidesdry <euclidesdry@gmail.com>
2021-12-20 23:14:21 +0100

    Definindo o Canvas e criando o Background

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 const CANVAS = document.querySelector('#playerground__snake');
+const CONTEXT = CANVAS.getContext('2d');
+let box = 32;
+
+function criarBackground() {
+    CONTEXT.fillStyle = "lightgreen";
+    CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);
+}
+
+criarBackground();
