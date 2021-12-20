ef8ba6da05632343b3d0622569135bf7c73c7c54
euclidesdry <euclidesdry@gmail.com>
2021-12-20 23:28:16 +0100

    Making the snake in canvas :snake:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,36 @@
 const CANVAS = document.querySelector('#playerground__snake');
 const CONTEXT = CANVAS.getContext('2d');
-let box = 32;
+let BOX = 32;
+let snake = [];
 
-function criarBackground() {
-    CONTEXT.fillStyle = "lightgreen";
-    CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);
+snake[0] = {
+    x: 8 * BOX,
+    y: 8 * BOX
+};
+
+/**
+ * @param {string} color Backgorund color;
+ * @param {number} squareSize Size of each the square;
+ *
+ * @description Create canvas background depending to the Backgorund color and square size;
+ */
+function makeBackground(color = "lightgreen", squareSize) {
+    CONTEXT.fillStyle = color;
+    CONTEXT.fillRect(0, 0, 16 * squareSize, 16 * squareSize);
 }
 
-criarBackground();
+/**
+ * @param {string} color Snake color;
+ * @param {number} squareSize Size of each the square;
+ *
+ * @description Create the snake with the color and square size and passed cordinates;
+ */
+function makeSnake(color = "lightgreen", squareSize) {
+    for(let i = 0; i < snake.length; i++) {
+        CONTEXT.fillStyle = color;
+        CONTEXT.fillRect(snake[i].x, snake[i].y, squareSize, squareSize);
+    }
+}
+
+makeBackground("lightgreen", BOX);
+makeSnake("green", BOX);
